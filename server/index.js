8ce1901b9f84bfe5d14be808fa2784f37e395182
SolomonOjigbo <solomonojigbo@gmail.com>
2022-07-23 12:07:21 +0100

    Ádded Client and Project Mongoose Models and GraphQL schemas

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const colors = require('colors');
 const cors = require('cors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 
 const port = process.env.PORT || 5000;
 const connectDB = require('./config/db');
@@ -15,7 +16,7 @@ connectDB();
 app.use(cors());
 
 app.use('/graphql', graphqlHTTP ({
-
+    schema,
     graphiql: process.env.NODE_ENV === 'development'
 
 }))
