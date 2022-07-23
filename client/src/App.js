a47dbe91e15a8e228402c06f7a226ad48f78eb76
SolomonOjigbo <solomonojigbo@gmail.com>
2022-07-23 18:06:04 +0100

    Setup ApolloClient on the client side and connected it to the backend server

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import Header from './components/Header';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache,
+});
 
 
 function App() {
   return (
-
     <>
+    <ApolloProvider client={client}>
     <Header />
       <div className="container">
       
       <h1>Project Management App</h1>
     </div>
+    </ApolloProvider>
+    
     </>
   
   );
