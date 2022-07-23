dd0c69ba8cb1927855e72a17c1a781078176ec98
SolomonOjigbo <solomonojigbo@gmail.com>
2022-07-23 17:51:48 +0100

    Initialized the client-side app with Create-react-app, installed some dependencies  and created the Header component

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+
+    <>
+    <Header />
+      <div className="container">
+      
+      <h1>Project Management App</h1>
+    </div>
+    </>
+  
+  );
+}
+
+export default App;
