676414aaf60a697771f775b14a0e095f02125477
SolomonOjigbo <solomonojigbo@gmail.com>
2022-07-26 03:51:10 +0100

    Added the EditProjectForm and DeleteProject Button components to the Project Details page

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -4,6 +4,8 @@ import { useQuery } from '@apollo/client';
 import { GET_PROJECT } from '../queries/projectQueries';
 
 import ClientInfo from '../components/ClientInfo';
+import EditProjectForm from '../components/EditProjectForm';
+import DeleteProjectButton from '../components/DeleteProjectButton';
 
 export default function Project() {
   const { id } = useParams();
@@ -26,6 +28,8 @@ export default function Project() {
           <h5 className='mt-3'>Project Status</h5>
           <p className='lead'>{data.project.status}</p>
           <ClientInfo client={data.project.client} />
+          <EditProjectForm project={data.project} />
+          <DeleteProjectButton projectId={data.project.id}/>
         </div>
       )}
     </>
